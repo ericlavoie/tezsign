662849c527f0fe518f6b79cae715a0cc9ea81bb4
v <cryi@tutanota.com>
2025-11-17 21:15:19 +0100

    apt update

--- armbian_userpatches/customize-image.sh
+++ armbian_userpatches/customize-image.sh
@@ -4,6 +4,7 @@ set -e
 
 # This command checks if the package status is NOT 'no such package' (i.e., it exists)
 # and then purges only the packages that are found.
+sudo apt update
 
 grep -Fvf <(dpkg-query -W -f='${Package}\n' | sed 's/^/#/') /tmp/overlay/packages_to_purge.txt | \
     xargs sudo apt purge --assume-yes
